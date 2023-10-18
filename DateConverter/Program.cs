d66c333dc5a1c11a2c2d39813ac03dc004f81070
Tak1Maker <salim.mouttaki@proton.me>
2023-10-18 14:24:15 +0200

    Initial commit

---
+++ DateConverter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+var listOfDatesOfNumbers = new List<string>() { "206" , "606", "2060", "307"};
+
+
+for(int i = 0; i < listOfDatesOfNumbers.Count; i++)
+{
+    if (listOfDatesOfNumbers[i].Length < 4)
+        listOfDatesOfNumbers[i] = "0" + listOfDatesOfNumbers[i];
+        
+    Console.WriteLine(DateTime.ParseExact(listOfDatesOfNumbers[i], "ddMM", CultureInfo.InvariantCulture, DateTimeStyles.None));
+}
+
